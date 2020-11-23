0693279c1fdbe74527308246299db4efc037445c
MakersAll8 <lixiao.shanghai@gmail.com>
2020-11-23 13:53:10 +1100

    minor changes

--- src/hooks/useTranslate.js
+++ src/hooks/useTranslate.js
@@ -6,7 +6,8 @@ export const useTranslate = () => {
   const [state, dispatch] = useContext(TranslateContext)
 
   useEffect(()=>{
-    // this is the mock up api call to get translated messages
+    // this is the mock up api to get translated messages
+    // theoretically, we can load only messages needed by passing in locale and message ids
     const getMessages = async ()=>{
       const res = await loadLocaleData(state.locale);
       if (res && res.default) dispatch({type:'SET_MESSAGES', payload:{messages: res.default}})
